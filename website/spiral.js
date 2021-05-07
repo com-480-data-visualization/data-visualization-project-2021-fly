c929c68000cf249ebc573750e73e9ecd6a5adfd3
Lilia Ellouz <lilia.ellouz@epfl.ch>
2021-05-07 15:59:24 +0200

    more spirals

--- website/spiral.js
+++ website/spiral.js
@@ -1,46 +1,7 @@
 var width = 960,
     height = 500;
 
-
-data = [{"Number": 847, "Event": "Elton John"}]
-         // ,
-         // {"Number": 0, "Event": "X"}]
-
-var centerX = width/2,
-    centerY = height/2,
-    radius = 170,
-    sides = 200,
-    coils = 4,
-    rotation = 0;
-
 var transitionDuration = 2000
-    
-// How far to step away from center for each side.
-var awayStep = radius/sides; 
-// How far to rotate around center for each side.
-var aroundStep = coils/sides;// 0 to 1 based.
-// Convert aroundStep to radians.
-var aroundRadians = aroundStep * 2 * Math.PI;
-// Convert rotation to radians.
-rotation *= 2 * Math.PI;
-
-
-var new_time = [];
-
-// For every side, step around and away from center.
-for(var i=100; i<=sides; i++){
-  // How far away from center
-  var away = i * awayStep ;
- 
-  // How far around the center.
-  var around = i * aroundRadians + rotation;
-
-  // Convert 'around' and 'away' to X and Y.
-  var x = centerX + Math.cos(around) * away;
-  var y = centerY + Math.sin(around) * away;
-  
-  new_time.push({x: x, y: y});
-}
 
 
 
@@ -60,21 +21,84 @@ FIRST SPIRAL
 
 
 
-var spiral_1 = d3.select("#spiral").append("svg")
+var svg = d3.select("#spiral").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g");
 
-path_1 = spiral_1.append("path")
-  .attr("d", lineFunction(new_time))
-  .attr("stroke", "url(#linear-gradient)")
-  // .attr("stroke", "red")
-  .attr("stroke-width", 10)
-  .attr("opacity", 0.5)
-  .attr("fill", "none")
+
+
+statistics = [{"Number": 847, "Event": "Elton John", "centerX": width/4, "centerY" : height/2, "radius": 170, "sides": 200, "coils": 3, "rotation":0},
+{"Number": 847, "Event": "Elton John", "centerX": 3*width/4, "centerY" : height/2, "radius": 170, "sides": 200, "coils": 2, "rotation":0}]
+         // ,
+         // {"Number": 0, "Event": "X"}]
+
+
+// path_1 = svg.append("path")
+//   .attr("d", lineFunction(new_time))
+//   .attr("stroke", "url(#linear-gradient)")
+//   // .attr("stroke", "red")
+//   .attr("stroke-width", 10)
+//   .attr("opacity", 0.5)
+//   .attr("fill", "none")
+
+svg.selectAll()    
+          .data(statistics)         
+      .enter().append("path")
+      .attr("d", function(d){
+
+        // How far to step away from center for each side.
+        var awayStep = d.radius/d.sides; 
+        // How far to rotate around center for each side.
+        var aroundStep = d.coils/d.sides;// 0 to 1 based.
+        // Convert aroundStep to radians.
+        var aroundRadians = aroundStep * 2 * Math.PI;
+        // Convert rotation to radians.
+        d.rotation *= 2 * Math.PI;
+
+        var new_time = [];
+
+        // For every side, step around and away from center.
+        for(var i=100; i<=d.sides; i++){
+          // How far away from center
+          var away = i * awayStep ;
+         
+          // How far around the center.
+          var around = i * aroundRadians + d.rotation;
+
+          // Convert 'around' and 'away' to X and Y.
+          var x = d.centerX + Math.cos(around) * away;
+          var y = d.centerY + Math.sin(around) * away;
+          
+          new_time.push({x: x, y: y});
+        }
+        return lineFunction(new_time);
+      })
+      .attr("stroke", "url(#linear-gradient)")
+      .attr("stroke-width", 10)
+      .attr("opacity", 0.5)
+      .attr("fill", "none")
+      .append("text")
+      .attr("y", function(d){return d.centerY - 20})
+     .attr("x", function(d){return d.centerX - 20})
+     .attr('text-anchor', 'middle')
+     .attr("class", "spiral-content")
+     .text("402")
+     .append("tspan")
+     // .attr("y", centerY+40)
+     .attr("y", function(d){
+        console.log(d.centerX);
+        return d.centerY + 40})
+     // .attr("x", centerX)
+     .attr("y", function(d){return d.centerX})
+     .text("entries")
+      .attr( "fill-opacity", 0 )
+      .transition()
+      .duration(transitionDuration )
+      .attr( "fill-opacity", 1 );
 
 // Define the gradient
-var linearGradient = spiral_1.append("defs")
+var linearGradient = svg.append("defs")
         .append("linearGradient")
         .attr("id", "linear-gradient")
         .attr("gradientTransform", "rotate(90)");
@@ -100,8 +124,8 @@ var linearGradient = spiral_1.append("defs")
         .attr("stop-color", color(5));
 
 // Transition
-var totalLength = path_1.node().getTotalLength();
-path_1
+var totalLength = svg.selectAll("path").node().getTotalLength();
+svg.selectAll("path")
 .attr("stroke-dasharray", totalLength + " " + totalLength)
 .attr("stroke-dashoffset", totalLength)
 .transition()
@@ -109,21 +133,31 @@ path_1
   .ease(d3.easeSin)
   .attr("stroke-dashoffset", 0);
 
+
+
+
+
+
 //Text
-spiral_1.append("text")
-   .attr("y", centerY - 20)
-   .attr("x", centerX)
-   .attr('text-anchor', 'middle')
-   .attr("class", "spiral-content")
-   .text("847")
-   .append("tspan")
-   .attr("y", centerY+40)
-   .attr("x", centerX)
-   .text("entries")
-    .attr( "fill-opacity", 0 )
-    .transition()
-    .duration(transitionDuration )
-    .attr( "fill-opacity", 1 );
+
+// svg.data(statistics)         
+//       .enter().append("text")
+//    .attr("y", function(d){return d.centerY - 20})
+//    .attr("x", function(d){return d.centerX - 20})
+//    .attr('text-anchor', 'middle')
+//    .attr("class", "spiral-content")
+//    .text("402")
+//    .append("tspan")
+//    // .attr("y", centerY+40)
+//    .attr("y", function(d){
+//       return d.centerY + 40})
+//    // .attr("x", centerX)
+//    .attr("y", function(d){return d.centerX})
+//    .text("entries")
+//     .attr( "fill-opacity", 0 )
+//     .transition()
+//     .duration(transitionDuration )
+//     .attr( "fill-opacity", 1 );
 
 
 /*
