6af42b315a9235de636c58040143fb1b3b0ff736
ghalifaten <faten.ghali@epfl.ch>
2021-05-27 14:11:41 +0200

    draw word cloud

--- website/word_cloud.js
+++ website/word_cloud.js
@@ -1,14 +1,33 @@
-import * as cloud from 'd3-cloud';
-
 const fontFamily = "Open Sans",
-    fontScale = d3.scaleLinear().range([20, 120]), // Construction d'une échelle linéaire continue qui va d'une font de 20px à 120px
+    fontScale = d3.scaleLinear().range([20, 60]), // Construction d'une échelle linéaire continue qui va d'une font de 20px à 120px
     fillScale = d3.scaleOrdinal(d3.schemeCategory10); // Construction d'une échelle discrète composée de 10 couleurs différentes
 
+var file = "files/word_count/1950_lyrics.csv"
+
+function draw(words) {
+d3.select("#word-cloud").append("svg") // Ajout d'un élément SVG sur un DIV existant de la page
+    .attr("class", "svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g") // Ajout du groupe qui contiendra tout les mots
+        .attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")") // Centrage du groupe
+        .selectAll("text")
+        .data(words)
+        .enter().append("text") // Ajout de chaque mot avec ses propriétés
+            .style("font-size", d => d.size + "px")
+            .style("font-family", fontFamily)
+            .style("fill", d => fillScale(d.size))
+            .attr("text-anchor", "middle")
+            .attr("transform", d => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
+            .text(d => d.text);
+}
+
+function update(file){
 //Read the data
-d3.csv("files/word_count/1950_lyrics.csv", function(csv) {
+d3.csv(file, function(csv) {
     var words = [];
     csv.forEach(function(e,i) {
-        words.push({"text": e.LABEL, "size": +e.COUNT});
+        words.push({"text": e.word, "size": +e.count});
     });
     words.length = 100; // Nous ne voulons que les 100 premiers mots
 
@@ -36,21 +55,11 @@ d3.csv("files/word_count/1950_lyrics.csv", function(csv) {
         .start();
 
     // La méthode draw
-    function draw() {
-    d3.select("#word-cloud").append("svg") // Ajout d'un élément SVG sur un DIV existant de la page
-        .attr("class", "svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g") // Ajout du groupe qui contiendra tout les mots
-            .attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")") // Centrage du groupe
-            .selectAll("text")
-            .data(words)
-            .enter().append("text") // Ajout de chaque mot avec ses propriétés
-                .style("font-size", d => d.size + "px")
-                .style("font-family", fontFamily)
-                .style("fill", d => fillScale(d.size))
-                .attr("text-anchor", "middle")
-                .attr("transform", d => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
-                .text(d => d.text);
-    }
-});
+    draw(words);
+})}
+
+function update_cloud(new_decade) {
+  d3.select(".svg").remove();
+  file = "files/word_count/"+new_decade+"_lyrics.csv";
+  update(file);
+}
