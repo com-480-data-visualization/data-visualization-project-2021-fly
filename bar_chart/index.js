c504e95eb7ff3dcf39c457e81a90794a6e66d15f
ghalifaten <faten.ghali@epfl.ch>
2021-05-29 20:20:00 +0200

    re-implement bar chart

--- bar_chart/index.js
+++ bar_chart/index.js
@@ -1,1 +1,60 @@
-export {default} from "./d43fa2a122cc9fbf@158.js";
+// set the dimensions of the cloud
+var sentiment = "love";
+var margin = {
+        top: 30,
+        right: 30,
+        bottom: 70,
+        left: 100
+    },
+    width = 1200 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svgBarChart = d3.select("#bar_chart_d3")
+                 .append("svg")
+                 .attr("width", width + margin.left + margin.right)
+                 .attr("height", height + margin.top + margin.bottom)
+
+var xScale = d3.scaleBand().range ([0, width]).padding(0.4),
+    yScale = d3.scaleLinear().range ([height, 0]);
+
+var g = svgBarChart.append("g")
+           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function update() {
+  file = "files/sentiment/"+sentiment+".csv"
+  d3.csv(file, function(error, data) {
+      if (error) {
+          throw error;
+      }
+
+      xScale.domain(data.map(function(d) { return d.decade; }));
+      yScale.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+      g.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(xScale));
+/*
+      g.append("g")
+       .call(d3.axisLeft(yScale).tickFormat(function(d){
+           return d;
+       }).ticks(5));
+*/
+
+      g.selectAll(".bar")
+       .data(data)
+       .enter().append("rect")
+       .attr("class", "bar")
+       .attr("x", function(d) { return xScale(d.decade); })
+       .attr("y", function(d) { return yScale(d.value); })
+       .attr("width", xScale.bandwidth())
+       .attr("height", function(d) { return height - yScale(d.value); });
+  });
+}
+
+function update_bar_chart(new_sentiment){
+  svgBarChart.selectAll(".bar").remove();
+  sentiment = new_sentiment;
+  update();
+}
+
+update();
--- website/bar_chart.js
+++ website/bar_chart.js
@@ -1,48 +1,60 @@
-var svg = d3.select("svg"),
-        margin = 200,
-        width = svg.attr("width") - margin,
-        height = svg.attr("height") - margin;
+// set the dimensions of the cloud
+var sentiment = "love";
+var margin = {
+        top: 30,
+        right: 30,
+        bottom: 70,
+        left: 100
+    },
+    width = 1200 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svgBarChart = d3.select("#bar_chart_d3")
+                 .append("svg")
+                 .attr("width", width + margin.left + margin.right)
+                 .attr("height", height + margin.top + margin.bottom)
 
 var xScale = d3.scaleBand().range ([0, width]).padding(0.4),
     yScale = d3.scaleLinear().range ([height, 0]);
 
-var g = svg.append("g")
-           .attr("transform", "translate(" + 100 + "," + 100 + ")");
+var g = svgBarChart.append("g")
+           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//load data
-d3.csv("files/sentiment/love.csv", function(error, data) {
-       if (error) {
-           throw error;
-       }
+function update() {
+  file = "files/sentiment/"+sentiment+".csv"
+  d3.csv(file, function(error, data) {
+      if (error) {
+          throw error;
+      }
 
-       xScale.domain(data.map(function(d) { return d.decade; }));
-       yScale.domain([0, d3.max(data, function(d) { return d.value; })]);
+      xScale.domain(data.map(function(d) { return d.decade; }));
+      yScale.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-       g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale));
+      g.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(xScale));
+/*
+      g.append("g")
+       .call(d3.axisLeft(yScale).tickFormat(function(d){
+           return d;
+       }).ticks(5));
+*/
 
-       g.append("g")
-        .call(d3.axisLeft(yScale))
-        .append("text")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("value");
+      g.selectAll(".bar")
+       .data(data)
+       .enter().append("rect")
+       .attr("class", "bar")
+       .attr("x", function(d) { return xScale(d.decade); })
+       .attr("y", function(d) { return yScale(d.value); })
+       .attr("width", xScale.bandwidth())
+       .attr("height", function(d) { return height - yScale(d.value); });
+  });
+}
 
-//add axes
-g.append("g")
- .attr("transform", "translate(0," + height + ")")
- .call(d3.axisBottom(xScale))
- .call(d3.axisLeft(yScale));
+function update_bar_chart(new_sentiment){
+  svgBarChart.selectAll(".bar").remove();
+  sentiment = new_sentiment;
+  update();
+}
 
-//add bars
-g.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return xScale(d.decade); })
-        .attr("y", function(d) { return yScale(d.value); })
-        .attr("width", xScale.bandwidth())
-        .attr("height", function(d) { return height - yScale(d.value); });
-});
+update();
